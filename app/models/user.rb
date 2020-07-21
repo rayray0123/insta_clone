07615defde813b5c3607bce6d4236b3538e984ec
rayray0123 <banapa2008@yahoo.co.jp>
2020-07-21 11:07:43 +0900

    バリデーションを書いた

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,15 @@
 #
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  # バリデーションは、正しいデータだけをデータベースに保存するために行われる
+  # パスワードは３文字以上で objectがdatabaseに保存されていないとき(new_record?)かつ changesがよく分からない
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  # パスワードが確認用パスワードと一致しいるかどうか
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  # 確認用パスワードが空じゃないか
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  # e-mailが空じゃないか、重複指定ないか
+  validates :email, presence: true, uniqueness: true
 end
