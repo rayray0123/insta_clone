1cbab8cea382025a5fc6946849a679fd986bde2b
rayray0123 <banapa2008@yahoo.co.jp>
2020-08-07 14:20:35 +0900

    application.cssにrequireを記入(bootstrap)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,7 @@
 
 //= require jquery3.js
+//= require popper
 //= require rails-ujs
+//= require activestorage
+//= require_tree .
 //= require bootstrap-material-design/dist/js/bootstrap-material-design.js
