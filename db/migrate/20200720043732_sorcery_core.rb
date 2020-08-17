84114cd0f42ee3466b1a258cfbc45888b79146c3
rayray0123 <banapa2008@yahoo.co.jp>
2020-08-17 10:22:25 +0900

    試していたmigrateファイルを元に戻す

--- db/migrate/20200720043732_sorcery_core.rb
+++ db/migrate/20200720043732_sorcery_core.rb
@@ -6,6 +6,7 @@ class SorceryCore < ActiveRecord::Migration[5.2]
 # ロールバックして何もmigrateしてない状態でdb:migrateすると今ある全てのmaigrationファイル(db/migrate内)が上から実行される
 # t.string :email, null: false, unique: true と書いてmigrateしてもunique: trueがつかない。なのでadd_index
 # ロールバックはマイグレーションファイルを書き換える前にしないとエラーが出る
+  def change
     create_table :users do |t|
       t.string :email, null: false
       t.string :username, null: false
@@ -16,6 +17,5 @@ class SorceryCore < ActiveRecord::Migration[5.2]
     end
     add_index :users, :email, unique: true
     add_index :users, :username, unique: true
-
   end
 end
