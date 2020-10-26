d70aa46fbe90556e1e9e63e6b8e1306b930c6301
rayray0123 <banapa2008@yahoo.co.jp>
2020-10-26 14:41:40 +0900

    フォローアンフォロー機能

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,15 @@
 class RelationshipsController < ApplicationController
+  before_action :require_login, only: %i[create destroy]
+
+  # _follow.html.slimのリンクからルーティング判断、createメソッド実行
   def create
+    # followしたいuserのidをparamsで取得
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
   end
 
   def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
   end
 end
