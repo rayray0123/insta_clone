1abb56d71abfd2f043a98a9bbdcababfdd5435e7
rayray0123 <banapa2008@yahoo.co.jp>
2021-03-03 10:45:59 +0900

    [Add]メール受信の設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.session_store :redis_store, servers: 'redis://localhost:6379', expire_after: 1.day
+
+  config.action_mailer.default_url_options = Settings.default_url_options.to_h
+  config.action_mailer.delivery_method = :letter_opener_web
 end
