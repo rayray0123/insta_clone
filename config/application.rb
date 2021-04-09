bfc5017c8aadba941ab074cd69369ab8a8fc3c99
rayray0123 <banapa2008@yahoo.co.jp>
2021-04-09 09:46:40 +0900

    [Add]sidekiqの設定

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module InstaClone
 
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    config.active_job.queue_adapter = :sidekiq
   end
   require 'active_model/railtie'
   require 'active_job/railtie'
